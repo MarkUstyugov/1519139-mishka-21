a82c1352ee3f65b49f2e0ac5028ee45f3644ce51
Mark Ustyugov <mark.ustyugov@gmail.com>
2020-12-21 23:05:50 +0500

    Резиновая верстка

--- source/js/menu.js
+++ source/js/menu.js
@@ -3,10 +3,16 @@ var mainNavigation = document.querySelector('.main-navigation');
 var search = document.querySelector('.search');
 var userNavigation = document.querySelector('.user-navigation');
 
+menuButton.classList.add("page-header__menu-toggle--show");
+menuButton.classList.add('page-header__menu-toggle--closed');
+mainNavigation.classList.add("main-navigation--active");
+search.classList.toggle('search--active');
+userNavigation.classList.toggle('user-navigation--active');
+
 menuButton.addEventListener('click', function(evt) {
   evt.preventDefault();
-  mainNavigation.classList.toggle('main-navigation--closed');
-  search.classList.toggle('search--closed');
-  userNavigation.classList.toggle('user-navigation--closed');
+  mainNavigation.classList.toggle('main-navigation--active');
+  search.classList.toggle('search--active');
+  userNavigation.classList.toggle('user-navigation--active');
   menuButton.classList.toggle('page-header__menu-toggle--closed');
 });
